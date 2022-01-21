7273236856d17b55d88fae330eba4ecbb20135fb
Fabian Kelvans <fkelvans@gmail.com>
2022-01-21 15:05:51 +0300

    Parallax Scroll Text-Effect

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+// Parallax Scroll Text Effect
+
+let atScroll = false;
+let parallaxTitle = document.querySelectorAll('.parallax-title');
+
+const scrollProgress = () => {
+    atScroll = true;
+}
+
+const raf = () => {
+    if(atScroll){
+        parallaxTitle.forEach((element, index) => {
+            element.style.transform = "translateX(" + window.scrollY / 8 + "%)";
+        });
+        atScroll = false;
+    }
+    requestAnimationFrame(raf);
+};
+
+
+requestAnimationFrame(raf);
+window.addEventListener('scroll', scrollProgress); 
